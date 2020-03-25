f2f79ddf66184d694baf634c57eb342df02d0d77
Oscargo01 <oscarand.gomez@urosario.edu.co>
2020-03-25 17:35:10 -0500

    Creacion segunda figura y frutos puestos en la casilla

--- Entrega3.py
+++ Entrega3.py
@@ -1,12 +1,19 @@
 import matplotlib.pyplot as plt
 from pylab import *
 import numpy as np
-diccionario = {'Q1':1, 'Q2':0, 'Q3':1 , 'Q4':0 ,  'P1':1 ,'P2':0 , 'P3':1 , 'P4':1, 'R1':1 , 'R2':0 , 'R3':0 , 'R4':1 , 'S1':1 , 'S2':1 , 'S3':1 , 'S4':0}
+import math as mt
+diccionario = {'M':1, 'M':1, 'M':1 , 'N':1 ,  'M':1 ,'M':1 , 'M':1 , 'N':1, 'M':1 , 'M':1 , 'M':1 , 'N':1 , 'P':1 , 'P':1 , 'P':1 , 'B':1}
 valores = diccionario.values()
 letras = diccionario.keys()
+letras.sort()
 fig = plt.figure(figsize=(4,4))
 for i in range(1,17):
-    ax = fig.add_subplot(4, 4, i, xticklabels=[], yticklabels=[], xticks=[], yticks=[])
-    
+    ax = fig.add_subplot(4, 4, i, xticklabels=[], yticklabels=[], xticks=[], yticks=[])  
 plt.savefig("Terreno_vacio.png")
+plt.show()
+fig2 = plt.figure(figsize=(4,4))
+for i in range(1,17):
+    ax = fig2.add_subplot(4, 4,i, xticklabels=[], yticklabels=[], xticks=[], yticks=[])  
+    plt.text(0.2, 0.4,letras[i%4])
+plt.savefig("Terreno_Con_Letras.png")
 plt.show()
